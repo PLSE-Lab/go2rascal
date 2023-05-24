d575c67db92dc1fd9115af7c033dea80570e326e
Mark Hills <hillsma@appstate.edu>
2023-05-24 15:35:14 -0400

    Allow location annotations to be enabled or disabled

--- go2rascal.go
+++ go2rascal.go
@@ -9,13 +9,13 @@ import (
 	"strings"
 )
 
-func processFile(filePath string) string {
+func processFile(filePath string, addLocs bool) string {
 	fset := token.NewFileSet()
 	file, err := parser.ParseFile(fset, filePath, nil, 0)
 	if err != nil {
 		return fmt.Sprintf("errorFile(Could not process file %s, %s)", filePath, err.Error())
 	} else {
-		return visitFile(file, fset)
+		return visitFile(file, fset, addLocs)
 	}
 }
 
@@ -31,60 +31,80 @@ func computeLocation(fset *token.FileSet, start token.Pos, end token.Pos) string
 	}
 }
 
-func visitFile(node *ast.File, fset *token.FileSet) string {
+func visitFile(node *ast.File, fset *token.FileSet, addLocs bool) string {
 	var decls []string
 	for i := 0; i < len(node.Decls); i++ {
-		decls = append(decls, visitDeclaration(&node.Decls[i], fset))
+		decls = append(decls, visitDeclaration(&node.Decls[i], fset, addLocs))
 	}
 	declString := strings.Join(decls, ",")
 
 	var imports []string
 	for i := 0; i < len(node.Imports); i++ {
-		imports = append(imports, visitImportSpec(node.Imports[i], fset))
+		imports = append(imports, visitImportSpec(node.Imports[i], fset, addLocs))
 	}
 	importString := strings.Join(imports, ",")
 
-	locationString := computeLocation(fset, node.FileStart, node.FileEnd)
-
 	packageName := node.Name.Name
-	return fmt.Sprintf("file(\"%s\", [%s], [%s], at=%s)", packageName, declString, importString, locationString)
+
+	if addLocs {
+		locationString := computeLocation(fset, node.FileStart, node.FileEnd)
+		return fmt.Sprintf("file(\"%s\", [%s], [%s], at=%s)", packageName, declString, importString, locationString)
+	} else {
+		return fmt.Sprintf("file(\"%s\", [%s], [%s])", packageName, declString, importString)
+	}
+
 }
 
-func visitDeclaration(node *ast.Decl, fset *token.FileSet) string {
+func visitDeclaration(node *ast.Decl, fset *token.FileSet, addLocs bool) string {
 	switch d := (*node).(type) {
 	case *ast.GenDecl:
-		return visitGeneralDeclaration(d, fset)
+		return visitGeneralDeclaration(d, fset, addLocs)
 	case *ast.FuncDecl:
-		return visitFunctionDeclaration(d, fset)
+		return visitFunctionDeclaration(d, fset, addLocs)
 	default:
 		return "" // This is an error, should panic here
 	}
 }
 
-func visitGeneralDeclaration(node *ast.GenDecl, fset *token.FileSet) string {
-	locationString := computeLocation(fset, node.Pos(), node.End())
-	return fmt.Sprintf("placeholderDecl(at=%s)", locationString)
+func visitGeneralDeclaration(node *ast.GenDecl, fset *token.FileSet, addLocs bool) string {
+	if addLocs {
+		locationString := computeLocation(fset, node.Pos(), node.End())
+		return fmt.Sprintf("placeholderDecl(at=%s)", locationString)
+	} else {
+		return fmt.Sprintf("placeholderDecl()")
+	}
 }
 
-func visitFunctionDeclaration(node *ast.FuncDecl, fset *token.FileSet) string {
-	locationString := computeLocation(fset, node.Pos(), node.End())
-	return fmt.Sprintf("placeholderDecl(at=%s)", locationString)
+func visitFunctionDeclaration(node *ast.FuncDecl, fset *token.FileSet, addLocs bool) string {
+	if addLocs {
+		locationString := computeLocation(fset, node.Pos(), node.End())
+		return fmt.Sprintf("placeholderDecl(at=%s)", locationString)
+	} else {
+		return fmt.Sprintf("placeholderDecl()")
+	}
 }
 
-func visitImportSpec(node *ast.ImportSpec, fset *token.FileSet) string {
-	locationString := computeLocation(fset, node.Pos(), node.End())
-	return fmt.Sprintf("placeholderImportSpec(at=%s)", locationString)
+func visitImportSpec(node *ast.ImportSpec, fset *token.FileSet, addLocs bool) string {
+	if addLocs {
+		locationString := computeLocation(fset, node.Pos(), node.End())
+		return fmt.Sprintf("placeholderImportSpec(at=%s)", locationString)
+	} else {
+		return fmt.Sprintf("placeholderImportSpec()")
+	}
 }
 
 func main() {
 	var filePath string
 	flag.StringVar(&filePath, "filePath", "", "The file to be processed")
 
+	var addLocations bool
+	flag.BoolVar(&addLocations, "addLocs", true, "Include location annotations")
+
 	flag.Parse()
 
 	if filePath != "" {
 		//fmt.Printf("Processing file %s\n", filePath)
-		fmt.Println(processFile(filePath))
+		fmt.Println(processFile(filePath, addLocations))
 	} else {
 		fmt.Println("No file given")
 	}
